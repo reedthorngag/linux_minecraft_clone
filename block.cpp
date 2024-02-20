861ee4cfe3829098ac0deee2a4a4b85e62f07e0f
Brody Cooper <reed.thorngag@gmail.com>
2024-02-21 11:44:15 +1300

    array drawing working

--- block.cpp
+++ block.cpp
@@ -6,22 +6,39 @@
 #include "block.hpp"
 
 const float Block::vertices[]{
-    0, 0.0, 0.0,    0, 0.01, // 0 bottom left front
-    0, 1.0, 0.0,    0, 0, // 1 top left front
-    1, 0.0, 0.0,    0.005, 0.01, // 2 bottom right front
-    1, 1.0, 0.0,    0.005, 0, // 3 top right front
-    0, 0.0, 1,      0, 0.01, // 4 bottom left back
-    0, 1.0, 1,      0, 0, // 5 top left back    
-    1, 0.0, 1.0,    0.005, 0.01, // 6 bottom right back
-    1, 1.0, 1.0,    0.005, 0, // 7 top right back
+    0, 0.0, 0.0,    0, 0, // 0 bottom right front
+    0, 1.0, 0.0,    0.007, 0, // 1 top right front
+    1, 0.0, 0.0,    0, 0.01, // 2 bottom left front
+    1, 1.0, 0.0,    0.007, 0.01, // 3 top left front
+    0, 0.0, 1,      0, 0.01, // 4 bottom right back
+    0, 1.0, 1,      0.007, 0.01, // 5 top right back    
+    1, 0.0, 1.0,    0, 0, // 6 bottom left back
+    1, 1.0, 1.0,    0.007, 0, // 7 top left back
 };
 const unsigned int Block::indices[]{
     0,1,2,
     2,1,3,
     4,5,6,
     6,5,7,
+    0,1,5,
     0,4,5,
-    0,1,5
+    2,6,7,
+    2,3,7,
+    1,3,7,
+    5,7,1
+};
+
+const float array[] {
+    0,0,0,
+    1,1,0,
+    1,0,0,
+    1,1,1,
+    1,0,1,
+    0,1,1,
+    0,0,1,
+    0,1,0,
+    0,0,0,
+    1,1,0
 };
 
 
@@ -31,9 +48,9 @@ void Block::render(unsigned int program) {
     glUniformMatrix4fv(glGetUniformLocation(program, "model"),1,false,&model[0][0]);
 
     glBindVertexArray(this->VAO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
-    glDrawElements(GL_TRIANGLES, sizeof(Block::indices), GL_UNSIGNED_INT, NULL);
-
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+    //glDrawElements(GL_TRIANGLES, sizeof(Block::indices), GL_UNSIGNED_INT, NULL);
+    glDrawArrays(GL_TRIANGLE_STRIP,0,10);
 }
 
 Block::Block() {
@@ -45,16 +62,16 @@ Block::Block() {
     glBindVertexArray(this->VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Block::vertices), Block::vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(array), array, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Block::indices), Block::indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
-    glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
+    //glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER,0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
